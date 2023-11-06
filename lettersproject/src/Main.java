5e81c1fe47b2c39485cadc5e5c3c76bca957be82
karina <karina.dyachenko06@gmail.com>
2023-11-06 21:37:13 +0600

    Adding player removal system

--- lettersproject/src/Main.java
+++ lettersproject/src/Main.java
@@ -9,21 +9,21 @@ public class Main
 
         System.out.println("Enter number of players");
         int playerNumber = scanner.nextInt();
-        String[] names = new String[playerNumber];
+        ArrayList<String> names = new ArrayList<>();
         System.out.println("Enter your names");
         scanner.nextLine();
         for (int i = 0; i < playerNumber; i++) {
-            names[i] = scanner.nextLine();
+            names.add(scanner.nextLine());
         }
         for (int i = 0; i < playerNumber; i++) {
             int index = random.nextInt(playerNumber);
-            String a = names[index];
-            names[index] = names[i];
-            names[i] = a;
+            String a = names.get(index);
+            names.set(index, names.get(i));
+            names.set(i, a);
         }
         System.out.println("You will be playing in following order:");
         for (int i = 0; i < playerNumber; i++) {
-            System.out.println(names[i]);
+            System.out.println(names.get(i));
         }
 
         ArrayList<String> myWords = new ArrayList<>();
@@ -52,111 +52,126 @@ public class Main
 
         int[] leaderboard = new int[playerNumber];
 
-
-
         int n = 0;
 
         boolean keepGoing = true;
-        for (int i = 0; i < myWords.size(); i++) {
-            char[] currentWord = myWords.get(i).toCharArray();
+        for (String myWord : myWords) {
+            char[] currentWord = myWord.toCharArray();
             char[] currentWordHidden = new char[currentWord.length];
             for (int k = 0; k < currentWord.length; k++) {
                 currentWordHidden[k] = '_';
                 System.out.print(currentWordHidden[k] + " ");
             }
             int playerPoints = 0;
-            System.out.println ("Guess the letter or the entire word");
-            System.out.println(names[0] + "'s turn to play.");
+
+            ArrayList<String> turns = new ArrayList<>();
+            for (int j = 0; j < names.size(); j++) {
+                turns.add(j, names.get(j));
+            }
+            int z = turns.size();
+            System.out.println("Guess the letter or the entire word");
+            System.out.println(turns.get(0) + "'s turn to play.");
             while (keepGoing) {
-                for (int l = 0; l < playerNumber; l++) { //change to turns size because of eliminations
+                for (int l = 0; l < z; l++) { //change to turns size because of eliminations
                     String playerGuess = scanner.nextLine();
                     if (playerGuess.length() == 1) {
-                        while (myWords.get(i).contains(playerGuess) && playerGuess.length() == 1) {
-                            System.out.println("Correct guess. Player " + names[l] + " gets another attempt");
+                        while (myWord.contains(playerGuess) && playerGuess.length() == 1) {
+                            System.out.println("Correct guess. Player " + turns.get(l) + " gets another attempt");
                             playerPoints += 100;
                             leaderboard[l] += playerPoints;
-                            playerPoints = 0;
-                            if (leaderboard[l] > myWords.get(i).length() * 100 / 2) {
+                            if (playerPoints > myWord.length() * 100 / 2) {
+                                playerPoints = 0;
                                 int o = l;
-                                for (int m = 1; m < playerNumber; m++) {
-                                    if (l == playerNumber - 1) {
-                                        System.out.println("Player" + names[l] + "has reached max pts. Each player gets 1 attempt to guess the full word. " + names[0] + "'s turn");
+                                for (int m = 1; m < z; m++) {
+                                    if (l == z - 1) {
+                                        System.out.println("Player" + turns.get(l) + "has reached max pts. Each player gets 1 attempt to guess the full word. " + turns.get(l + 1) + "'s turn");
                                         l = 0;
                                     }
                                     else {
-                                        System.out.println("Player" + names[l] + "has reached max pts. Each player gets 1 attempt to guess the full word. " + names[l + 1] + "'s turn");
+                                        System.out.println("Player" + turns.get(l) + "has reached max pts. Each player gets 1 attempt to guess the full word. " + turns.get(l + 1) + "'s turn");
                                         l++;
                                     }
                                     playerGuess = scanner.nextLine();
-                                    if (playerGuess.equalsIgnoreCase(myWords.get(i))) {
-                                        playerPoints += 100 * myWords.get(i).length();
+                                    if (playerGuess.equalsIgnoreCase(myWord)) {
+                                        playerPoints += 100 * myWord.length();
                                         leaderboard[m] += playerPoints;
-                                        System.out.println("Congratulations! Player " + names[l] + " won");
+                                        System.out.println("Congratulations! Player " + turns.get(l) + " won");
                                         keepGoing = false;
                                         playerPoints = 0;
-                                        l += playerNumber;
+                                        l += z;
                                         break;
                                     }
                                     else {
-                                        System.out.println("Wrong guess. " + names[l] + " lost");
+                                        System.out.println("Wrong guess. " + turns.get(l) + " lost");
                                     }
-                                    if (m == playerNumber - 1) {
-                                        System.out.println("Nobody has guessed right. " + names[o] + " wins.");
+                                    if (m == z - 1) {
+                                        System.out.println("Nobody has guessed right. " + turns.get(o) + " wins.");
                                         keepGoing = false;
-                                        l += playerNumber;
+                                        l += z;
                                         break;
                                     }
                                 }
                                 keepGoing = false;
-                                l += playerNumber;
+                                l += z;
                                 break;
                             }
                             playerGuess = scanner.nextLine();
                             if (playerGuess.length() > 1) {
-                                if (playerGuess.equalsIgnoreCase(myWords.get(i))) {
-                                    playerPoints += 100 * myWords.get(i).length();
+                                if (playerGuess.equalsIgnoreCase(myWord)) {
+                                    playerPoints += 100 * myWord.length();
                                     leaderboard[l] += playerPoints;
-                                    System.out.println("Congratulations! Player " + names[l] + "won");
+                                    System.out.println("Congratulations! Player " + turns.get(l) + " won");
                                     keepGoing = false;
                                     playerPoints = 0;
-                                    l += playerNumber;
-                                }
+                                    l += z;
+                                } 
                                 else {
-                                    if (l == playerNumber - 1) {
-                                        System.out.println("Wrong guess. Player eliminated. Next turn: " + names[0]);
+                                    if (l == z - 1) {
+                                        System.out.println("Wrong guess. Player eliminated. Next turn: " + turns.get(0));
+                                        turns.remove(l);
+                                        l--;
+                                        z--;
                                     }
                                     else {
-                                        System.out.println("Wrong guess. Player eliminated. Next turn: player " + names[l + 1]); //turns.get(l)
+                                        System.out.println("Wrong guess. Player eliminated. Next turn: player " + turns.get(l + 1)); //turns.get(l)
+                                        turns.remove(l);
+                                        l--;
+                                        z--;
                                     }
                                     break;
-
                                 }
                             }
                         }
-                        if (!myWords.get(i).contains(playerGuess) && playerGuess.length() == 1) {
-                            if (l == playerNumber - 1) {
-                                System.out.println("Wrong. Next turn: " + names[0]);
+                        if (!myWord.contains(playerGuess) && playerGuess.length() == 1) {
+                            if (l == z - 1) {
+                                System.out.println("Wrong. Next turn: " + turns.get(0));
                             }
                             else {
-                                System.out.println("Wrong guess. Next turn: " + names[l + 1]); //turns.get(l)
+                                System.out.println("Wrong guess. Next turn: " + turns.get(l + 1)); //turns.get(l)
                             }
                         }
                     }
                     else {
-                        if (playerGuess.equalsIgnoreCase(myWords.get(i))) {
-                            playerPoints += 100 * myWords.get(i).length();
+                        if (playerGuess.equalsIgnoreCase(myWord)) {
+                            playerPoints += 100 * myWord.length();
                             leaderboard[l] += playerPoints;
-                            System.out.println("Congratulations! Player " + "won");
+                            System.out.println("Congratulations! Player " + turns.get(l) + "won");
                             keepGoing = false;
                             playerPoints = 0;
                             break;
                         }
                         else {
-                            if (l == playerNumber - 1) {
-                                System.out.println("Wrong guess. Player eliminated. Next turn: " + names[0]);
+                            if (l == z - 1) {
+                                System.out.println("Wrong guess. Player eliminated. Next turn: " + turns.get(0));
+                                turns.remove(l);
+                                l--;
+                                z--;
                             }
                             else {
-                                System.out.println("Wrong guess. Player eliminated. Next turn: player " + names[l + 1]); //turns.get(l)
+                                System.out.println("Wrong guess. Player eliminated. Next turn: player " + turns.get(l + 1)); //turns.get(l)
+                                turns.remove(l);
+                                l--;
+                                z--;
                             }
                         }
                     }
@@ -166,7 +181,7 @@ public class Main
         }
         System.out.println("Game finished. Leaderboard:");
         for (int i = 0; i < leaderboard.length; i++) {
-            System.out.println(names[i] + ": " + leaderboard[i] + " pts");
+            System.out.println(names.get(i) + ": " + leaderboard[i] + " pts");
         }
     }
 }
